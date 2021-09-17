28a2bcf09f79c78c6b0bb513a00bd7da73e0cfcc
Pig Fang <g-plane@hotmail.com>
2021-09-17 09:20:27 +0800

    add type casts

--- src/Models/WhitelistItem.php
+++ src/Models/WhitelistItem.php
@@ -5,4 +5,10 @@ use Illuminate\Database\Eloquent\Model;
 class WhitelistItem extends Model
 {
     protected $table = 'moderation_whitelist';
+
+    protected $casts = [
+        'id' => 'integer',
+        'user_id' => 'integer',
+        'operator' => 'integer',
+    ];
 }
