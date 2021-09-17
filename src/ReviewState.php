02cf6ac79cd9263a3b40b5d4b9f8d0805b161ebb
Pig Fang <g-plane@hotmail.com>
2021-09-17 09:06:06 +0800

    fix syntax errors

--- src/ReviewState.php
+++ src/ReviewState.php
@@ -7,6 +7,6 @@ final class ReviewState
     public const MANUAL = 0;
     public const ACCEPTED = 1;
     public const REJECTED = 2;
-    public const USER = 3
-    public const MISS = 4
+    public const USER = 3;
+    public const MISS = 4;
 }
